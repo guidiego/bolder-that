c783ba1637fc22b9d33fcd1602fd860a5a0923d5
Guilherme Diego Albino Francisco <guidiego.expgames@gmail.com>
2015-08-31 21:08:57 -0300

    Update into the Gulpfile and Index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var gulp   = require("gulp"),
 		uglify = require("gulp-uglify"),
-		babel  = require("gulp-babel");	
+		babel  = require("gulp-babel");
 
 gulp.task("default", ["build"], function(){
 	gulp.watch("dev/**/*", ["build"]);
-})
+});
 
 gulp.task("build", ["babel"], function (){
 	console.log("Build successifully completed");
-})
+});
 
 gulp.task("babel", function () {
 	return gulp.src("dev/bolder-that.js")
 					.pipe(babel())
 					.pipe(uglify())
 					.pipe(gulp.dest("dist"));
-})
+});
